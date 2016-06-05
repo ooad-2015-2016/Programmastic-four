92d21d4150bc33fff1bfef70f2cedcba01824549
Amina Aljicevic <aljicevicamina@gmail.com>
2016-06-06 01:38:04 +0200

    Nisu se uspjele pozvati komande Install-Package EntityFramework.SQLite -Version 7.0.0-beta6 i ostele.

---
+++ _OoadProjekatBaza/_OoadProjekatBaza/KinoBaza/Models/RestoranDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _OoadProjekatBaza.KinoBaza.Models
+{
+    class RestoranDbContext:DbContext
+    {
+        public DbSet<Kino> Restorani { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string databaseFilePath = "Ooadbaza.db";
+            try
+            {
+                //za tačnu putanju gdje se nalazi baza uraditi ovdje debug i procitati Path
+                databaseFilePath = Path.Combine(ApplicationData.Current.LocalFolder.Path,
+               databaseFilePath);
+            }
+            catch (InvalidOperationException) { }
+            //Sqlite baza
+            optionsBuilder.UseSqlite($"Data source={databaseFilePath}");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //jedno od polja je image da se zna šta je zapravo predstavlja byte[]
+            modelBuilder.Entity<Kino>().Property(p => p.Slika).HasColumnType("image");
+        }
+    }
+}
